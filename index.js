b9392db9c41e911bf98de809daa6182452422699
Akash Y <yellgetti.akash@gmail.com>
2023-02-26 11:23:04 +0000

    changes for api and socket connect

--- index.js
+++ index.js
@@ -4,48 +4,40 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, {
+  transports: ['websocket', 'polling'],
   cors: {
-    origin: "http://web.itslets.com",
+    origin: "https://web.itslets.com",
     methods: ["GET", "POST"]
   }
 });
 
 
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/socket.html');
+});
+
+
  
-io.on('connection', (socket) => {  
+const socketIO = (socket) => {
+  console.log('a user connected');
 
-  console.log('connected')
-  socket.on('sendnotifi', function(dataArr){
-		//io.emit('sendnotifi', dataArr);
-		io.to(dataArr.socket_id).emit('sendnotifi', dataArr);
-  });  
-  socket.on('new_lets', function(dataArr){
-	  io.to(dataArr.socket_id).emit('new_lets', dataArr);
-  });  
-  socket.on('newaccepted', function(dataArr){
-	  io.to(dataArr.socket_id).emit('newaccepted', dataArr);
-  });  
-  socket.on('acceptpartner', function(dataArr){
-    io.to(dataArr.socket_id).emit('acceptpartner', dataArr);
-  });  
-  socket.on('rejectPartner', function(dataArr){
-	  io.to(dataArr.socket_id).emit('rejectPartner', dataArr);
-  });  
-  socket.on('cancelPartner', function(dataArr){
-	  io.to(dataArr.socket_id).emit('cancelPartner', dataArr);
-  });  
-  socket.on('cancelbypartner', function(dataArr){
-	  io.to(dataArr.socket_id).emit('cancelbypartner', dataArr);
-  });  
-  socket.on('rejectaccepted', function(dataArr){
-	  io.to(dataArr.socket_id).emit('rejectaccepted', dataArr);
-  });  
-  socket.on('cancelaccepted', function(dataArr){
-	  io.to(dataArr.socket_id).emit('cancelaccepted', dataArr);
-  });  
-}); 
+  socket.on('chat message', msg => {
+    console.log('chat message', msg);
+    io.emit('chat message', msg);
+  });
 
-var port =  process.env.PORT || 3000;
-server.listen(port, function(){
-  console.log('node is running on '+ port);
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+}
+
+
+
+
+io.on('connection', socketIO);
+
+
+const port =  process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log('listening on :'+port);
 });
